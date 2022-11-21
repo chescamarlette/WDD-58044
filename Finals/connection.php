00ea01fa75912c6c32b04807827a828ac89df5b2
chescamarlette <113018204+chescamarlette@users.noreply.github.com>
2022-11-21 22:11:57 +0800

    Update connection.php

--- Finals/connection.php
+++ Finals/connection.php
@@ -5,9 +5,9 @@ $username = "root";
 $password = "";
 $conn = mysqli_connect($server,$username,$password,$db);
 if($conn)
-{
-		echo "";
-	
+{	
+		// removed the database is connected since it will show in the form
+		echo ""; 
 }
 else
 {
